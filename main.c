d3c8a26dedb41c10d7c3aa502257c9b845745f6f
optima211 <nedmk@mail.ru>
2018-12-05 23:14:45 +0700

    structure & write success (with 3 warning)

--- main.c
+++ main.c
@@ -1,6 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+struct person
+{
+    char name[16];
+    int age;
+};
+
+int save(char * filename, struct person *p);
+int load(char * filename);
+
+int main(void)
+{
+    char * filename = "person.dat";
+    struct person tom = { "Tom", 21 };
+
+    save(filename, &tom);
+    load(filename);
+
+    return 0;
+}
+
+// запись структуры в файл
+int save(char * filename, struct person *p)
+{
+    FILE * fp;
+    char *c;
+    int size = sizeof(struct person); // количество записываемых байтов
+
+    if ((fp = fopen(filename, "wb")) == NULL)
+    {
+        perror("Error occured while opening file");
+        return 1;
+    }
+    // устанавливаем указатель на начало структуры
+    c = (char *)p;
+    // посимвольно записываем в файл структуру
+    for (int i = 0; i < size; i++)
+    {
+        putc(*c++, fp);
+    }
+    fclose(fp);
+    return 0;
+}
+
+// загрузка из файла структуры
+int load(char * filename)
+{
+    FILE * fp;
+    char *c;
+    int i; // для считывания одного символа
+    // количество считываемых байтов
+    int size = sizeof(struct person);
+    // выделяем память для считываемой структуры
+    struct person * ptr = (struct person *) malloc(size);
+
+    if ((fp = fopen(filename, "rb")) == NULL)
+    {
+        perror("Error occured while opening file");
+        return 1;
+    }
+
+    // устанавливаем указатель на начало блока выделенной памяти
+    c = (char *)ptr;
+    // считываем посимвольно из файла
+    while ((i = getc(fp))!=EOF)
+    {
+        *c = i;
+        c++;
+    }
+
+    fclose(fp);
+    // вывод на консоль загруженной структуры
+    printf("%-20s %5d \n", ptr->name, ptr->age);
+    free(ptr);
     return 0;
 }
